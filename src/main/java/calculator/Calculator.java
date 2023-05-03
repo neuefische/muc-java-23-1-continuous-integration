61549ad77b50807159fd6020b072170d2bfba79f
Sergej Grilborzer <sergej@grilborzer.de>
2023-05-03 14:40:09 +0200

    Create test example

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,9 @@
+package calculator;
+
+// Klasse die wir testen
+public class Calculator {
+	
+	public static int sum(int a, int b) {
+		return a + b;
+	}
+}
---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,56 @@
+package calculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+// Test der Klasse
+class CalculatorTest {
+
+	// "Annotation" fängt mit @
+	// @ auf Mac = option + l
+	@Test
+	// Was wollen wir testen?
+	// Wird ein richtiges Ergebnis zurück gegeben, wenn die Zahlen positiv sind?
+	// Testnamen + should ... + when ...
+	// summeSollteGültigesErgebnisZurückgeben_wennZahlenPositivSind
+	
+	// Ausführen mit rechtsclick -> run as junit
+	void sumShouldReturnValidResult_whenNumbersArePositive() {
+		// GIVEN - Mit diesen Parametern
+		int a = 1;
+		int b = 1;
+		// Erwartetes Ergebnis
+		int expected = 2;
+		
+		// WHEN - Soll...
+		// Tatsächliches Ergebnis
+		int result = Calculator.sum(a, b);
+		
+		// THEN - Erwartetes Ergebnis rauskommen
+		// Sicherstellen das expected = result ist
+		assertEquals(expected, result);
+		
+		Assertions.assertEquals(expected, result);
+	}
+
+	@Test
+	void sumShouldReturnValidResult_whenNumbersArePositive2() {
+		// GIVEN - Mit diesen Parametern
+		int a = 1;
+		int b = 1;
+		// Erwartetes Ergebnis
+		int expected = 2;
+		
+		// WHEN - Soll...
+		// Tatsächliches Ergebnis
+		int result = Calculator.sum(a, b);
+		
+		// THEN - Erwartetes Ergebnis rauskommen
+		// Sicherstellen das expected = result ist
+		assertEquals(expected, result);
+		
+		Assertions.assertEquals(expected, result);
+	}
+}
